76437c1092c1266d9287ee141a6e253117e9ee3d
Krzysztof Duśko <dusko.krzysztof@gmail.com>
2021-09-18 21:52:27 +0200

    First benchmark

---
+++ Benchmark.ReadBench-report-github.md
@@ -0,0 +1,14 @@
+``` ini
+
+BenchmarkDotNet=v0.13.1, OS=Windows 10.0.19043.1237 (21H1/May2021Update)
+Intel Core i5-7500 CPU 3.40GHz (Kaby Lake), 1 CPU, 4 logical and 4 physical cores
+.NET SDK=5.0.401
+  [Host]     : .NET 5.0.10 (5.0.1021.41214), X64 RyuJIT
+  DefaultJob : .NET 5.0.10 (5.0.1021.41214), X64 RyuJIT
+
+
+```
+|   Method |      FileName |     Mean |   Error |  StdDev |      Gen 0 |     Gen 1 |     Gen 2 | Allocated |
+|--------- |-------------- |---------:|--------:|--------:|-----------:|----------:|----------:|----------:|
+| **ReadFile** | **200kFile.xlsb** | **270.4 ms** | **0.83 ms** | **0.78 ms** | **17000.0000** | **7000.0000** | **3000.0000** |    **105 MB** |
+| **ReadFile** | **200kFile.xlsx** | **797.0 ms** | **5.08 ms** | **4.24 ms** |  **8000.0000** | **4000.0000** | **2000.0000** |     **38 MB** |
---
+++ source/Benchmark/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using BenchmarkDotNet.Attributes;
+using BenchmarkDotNet.Running;
+using SpreadSheetTasks;
+
+namespace Benchmark
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+#if RELEASE
+            var summary = BenchmarkRunner.Run<ReadBench>();
+#endif
+#if DEBUG
+            var b1 = new ReadBench();
+            b1.FileName = "200kFile.xlsb";
+            Stopwatch st = new Stopwatch();
+            st.Start();
+            b1.ReadFile();
+            Console.WriteLine(st.ElapsedMilliseconds);
+#endif
+
+        }
+    }
+
+    [MemoryDiagnoser]
+    public class ReadBench
+    {
+
+        [Params("200kFile.xlsx", "200kFile.xlsb")]
+        public string FileName { get; set; }
+
+        [Benchmark]
+        public void ReadFile()
+        {
+#if RELEASE
+            var path = Path.Combine(Directory.GetCurrentDirectory(), $"..\\..\\..\\..\\..\\..\\..\\FilesToTest\\{FileName}");
+#endif
+#if DEBUG
+            var path = Path.Combine(Directory.GetCurrentDirectory(), $"..\\..\\..\\FilesToTest\\{FileName}");
+#endif
+
+            using (XlsxOrXlsbReadOrEdit excelFile = new XlsxOrXlsbReadOrEdit())
+            {
+                excelFile.Open(path);
+                excelFile.ActualSheetName = "sheet1";
+                object[] row = null;
+                int rowNum = 0;
+                while (excelFile.Read())
+                {
+                    if (row == null)
+                    {
+                        row = new object[excelFile.FieldCount];
+                    }
+                    excelFile.GetValues(row);
+                    rowNum++;
+#if DEBUG
+                    if (rowNum % 10_000 == 0)
+                        Console.WriteLine("row " + rowNum.ToString("N0") + ": " + String.Join('|', row));
+#endif
+                }
+            }
+        }
+    }
+}
